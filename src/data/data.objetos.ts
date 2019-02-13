65f36d7c8a3d7bda9ed6a17bbb18cb24037fe48a
Luis Rojas <chikavi@Santiagos-Mac-mini-2.local>
2019-02-13 12:40:42 -0600

    empezando proyecto

---
+++ src/data/data.objetos.ts
@@ -0,0 +1,62 @@
+export const OBJETOS = [
+{
+	codigobarra: "7501",
+	img: "https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg",
+	cantidad: 1,
+	departamento: "comida",
+	nombre: "Pizzas",
+	costo: 123,
+	precio: 130,
+	descripcion: "loremsdad asdas dsa da 3 ewqdeasda asdasdgdf asdasda"
+},
+{
+	codigobarra: "123a",
+	img: "https://cdn-3.expansion.mx/dims4/default/f891dc2/2147483647/strip/true/crop/1254x837+0+0/resize/800x534!/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Ff6%2Fe3%2F6925673440018c64171304d0bf70%2Fistock-520704188.jpg",
+	cantidad: 2,
+	departamento: "comida",
+	nombre: "Cervezas",
+	costo: 123,
+	precio: 130,
+	descripcion: "loremsdad asdas dsa da 3 ewqdeasda asdasdgdf asdasda"
+},
+{
+	codigobarra: "75012",
+	img: "http://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/01/ensalada11.jpg",
+	cantidad: 3,
+	departamento: "comida",
+	nombre: "Ensaladas",
+	costo: 123,
+	precio: 130,
+	descripcion: "2"
+},
+{
+	codigobarra: "750123",
+	img: "https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg",
+	cantidad: 4,
+	departamento: "comida",
+	nombre: "refresco",
+	costo: 123,
+	precio: 130,
+	descripcion: "4"
+},
+{
+	codigobarra: "7501234",
+	img: "https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg",
+	cantidad: 5,
+	departamento: "comida",
+	nombre: "carnita",
+	costo: 123,
+	precio: 130,
+	descripcion: "loremsdad asdas dsa da 3 ewqdeasda asdasdgdf asdasda"
+},
+{
+	codigobarra: "75012345",
+	img: "https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg",
+	cantidad: 6,
+	departamento: "comida",
+	nombre: "pizza",
+	costo: 123,
+	precio: 130,
+	descripcion: "loremsdad asdas dsa da 3 ewqdeasda asdasdgdf asdasda"
+},
+];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { ToastController } from 'ionic-angular';
+import { OBJETOS } from '../../data/data.objetos';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+	objetos:any[] = [];
+texto = "resultado";
+items: any;
+  constructor(public navCtrl: NavController,
+		  	private barcodeScanner: BarcodeScanner,
+		  	private toastCtrl: ToastController) {
+  	this.objetos = OBJETOS.slice(0);
+  	 this.initializeItems();
   }
 
+  scan(){
+  	this.barcodeScanner.scan().then(barcodeData => {
+  		this.mostrarError("Buscando...");
+  		this.getItems(barcodeData.text);
+	}).catch(err => {
+	    console.log('Error', err);
+	    this.mostrarError("Error: " + err)
+	});
+	  }
+
+ initializeItems() {
+    this.items = OBJETOS.slice(0);
+  }
+
+getItems(ev: string) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+buscar(resultado:string){
+	this.texto = resultado;
+}
+
+mostrarError( mensaje : string ){
+	 let toast = this.toastCtrl.create({
+    message: mensaje
+ ,   duration: 1500,
+    position: 'bottom'
+  });
+	 toast.present();
+
+}
 }
