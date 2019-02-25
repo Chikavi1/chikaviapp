f74895b1069b0ba96c1f59be2b543a335cbd7c1e
chikavi1 <chikavi@hotmail.com>
2019-02-25 09:40:40 -0600

    agregue la vista y la logica al hacer un pedido

---
+++ src/pages/pedidos/pedidos.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams, ModalController } from 'ionic-angular';
+import { PedidosProvider } from '../../providers/pedidos/pedidos';
+import { ModalPage } from '../modal/modal';
+	
+/**
+ * Generated class for the PedidosPage page.
+ *	
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-pedidos',
+  templateUrl: 'pedidos.html',
+})
+export class PedidosPage {
+	pedidos :any[];
+  mesa:any = {};
+  nmesa;
+  data;
+
+ 
+  constructor(public navCtrl: NavController,
+             public navParams: NavParams,
+             public Pedidos:PedidosProvider,
+             private modalCtrl: ModalController) {
+    this.mesa = this.navParams.get("mesa");
+    this.nmesa = this.mesa.id;
+    console.log(this.nmesa);
+    this.data = {
+      "folio":"1234",
+      "cantidad":"3",
+      "mesa": this.nmesa,
+      "status": 0,
+      "vendedor":"luis rojas",
+      "nombre":"pizza",
+      "products_id":1,
+      "precio":"220"
+  }
+  console.log(this.data);
+
+
+   	 this.Pedidos.index(this.nmesa).subscribe(
+      (data) => {this.pedidos = data},
+       (error) =>{console.log(error)}
+       );
+
+  }
+
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PedidosPage');
+  }
+borrarProducto(producto:any,idx:number){
+  this.pedidos.splice(idx,1);
+  this.Pedidos.eliminarProducto(producto.id);
+}
+ 
+mostrar_modal(){
+  let modal = this.modalCtrl.create(ModalPage,{ "numero_mesa" : this.nmesa });
+  modal.present();
+  modal.onDidDismiss( parametros => {
+    console.log("datos del modal");
+    console.log(parametros);
+  })
+}
+
+ generarPedido() {
+    this.Pedidos.post(this.data).then((result) => {
+    console.log(result);
+  }, (err) => {
+    console.log(err);
+  });
+}
+
+doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      this.Pedidos.index(this.nmesa).subscribe(
+      (data) => {this.pedidos = data},
+      (error) => {}
+      //console.log(data)
+      
+      );
+            refresher.complete();
+    }, 2000);
+  }
+
+}
