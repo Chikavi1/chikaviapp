cb7a9779aceb657b7a2c25dfb5fe17628f0d4b13
chikavi1 <chikavi@hotmail.com>
2019-02-25 09:38:44 -0600

    agregue la informacion de las mesas

---
+++ src/data/mesas.ts
@@ -0,0 +1,32 @@
+export const MESAS = [
+{
+	img: "https://image.freepik.com/free-photo/selected-focus-empty-brown-wooden-table-coffee-shop-restaurant-blur-background-with-bokeh-image-your-photomontage-product-display_1423-1459.jpg",
+	id: 1	
+},
+
+{
+	img: "https://us.123rf.com/450wm/kaisorn/kaisorn1703/kaisorn170300089/74215173-tablero-de-madera-de-la-mesa-en-el-fondo-del-caf%C3%A9-del-bokeh-de-la-falta-de-definici%C3%B3n-se-puede-utilizar-p.jpg?ver=6",
+	id: 2	
+},
+
+{
+	img: "https://st2.depositphotos.com/4134095/8670/i/950/depositphotos_86701296-stock-photo-counter-bar-table-restaurant-background.jpg",
+	id: 3	
+},
+
+{
+	img: "https://image.freepik.com/free-photo/selected-focus-empty-brown-wooden-table-coffee-shop-restaurant-blur-background-with-bokeh-image-your-photomontage-product-display_1423-1459.jpg",
+	id: 4	
+},
+
+{
+	img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRtDDA62fvRti2U2bByffQ-UafLjNuLgcENqFSs9463SIYdoJh",
+	id: 5	
+},
+
+{
+	img: "https://i.pinimg.com/originals/52/b0/46/52b046bc4a5ce2e5f20ec1337cbcdcce.jpg",
+	id: 6	
+},
+
+];
